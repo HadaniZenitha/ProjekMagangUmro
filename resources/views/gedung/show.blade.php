c53efbc81ac243a2cfb45852e02ddf14846c8790
Hadani Zenitha Pinkan <hadanialaina@gmail.com>
2026-03-11 10:52:51 +0700

    Refactor gedung detail view layout

--- resources/views/gedung/show.blade.php
+++ resources/views/gedung/show.blade.php
@@ -2,29 +2,18 @@
 
 @section('content')
 <div class="container">
+    <h2>Detail Gedung</h2>
 
-<<<<<<< HEAD
-    <div class="d-flex justify-content-between align-items-center mb-3">
-        <h2>Detail Gedung</h2>
-
-        <a href="{{ route('gedung.index') }}" class="btn btn-secondary">
-            <i class="fa-solid fa-arrow-left me-1"></i> Kembali
-        </a>
-    </div>
-
-=======
->>>>>>> d7302947f020310c79f6a86c9bbc92fdfa6339cf
     <ul class="list-group">
         <li class="list-group-item">
             <b>Kode:</b> {{ $gedung->kode_gedung }}
         </li>
-
         <li class="list-group-item">
             <b>Nama:</b> {{ $gedung->nama_gedung }}
         </li>
-
         <li class="list-group-item">
             <b>Status:</b>
+
             @if($gedung->is_active)
                 <span class="badge bg-success ms-2">
                     <i class="fa-solid fa-circle-check me-1"></i> Aktif
@@ -34,8 +23,11 @@
                     <i class="fa-solid fa-circle-xmark me-1"></i> Nonaktif
                 </span>
             @endif
-        </li>
+        </li>    
     </ul>
 
+    <a href="{{ route('gedung.index') }}" class="btn btn-secondary mt-3">
+        <i class="fa-solid fa-arrow-left me-1"></i>Kembali
+    </a>
 </div>
 @endsection
