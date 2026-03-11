3447974ad3ecba2d2bb78141a441a83f3bfb8891
Hadani Zenitha Pinkan <hadanialaina@gmail.com>
2026-03-11 11:16:59 +0700

    Fix merge conflict in create.blade.php
    Removed merge conflict markers from create.blade.php

--- resources/views/pic/create.blade.php
+++ resources/views/pic/create.blade.php
@@ -6,11 +6,8 @@
 
 <div class="d-flex justify-content-between align-items-center mb-4">
     <h5 class="fw-bold mb-0">Tambah PIC</h5>
-<<<<<<< HEAD
-=======
     <a href="{{ route('pic.index') }}">
     </a>
->>>>>>> d7302947f020310c79f6a86c9bbc92fdfa6339cf
 </div>
 
 <div class="card shadow-sm border-0">
@@ -70,7 +67,6 @@
                 <button class="btn btn-warning">
                     <i class="fa-solid fa-save me-1"></i>Simpan
                 </button>
-
                 <a href="{{ route('pic.index') }}" class="btn btn-danger">
                     Batal
                 </a>
