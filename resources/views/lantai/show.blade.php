d4fd4d32d8a5735fe6b32255de159774e7b588b9
Hadani Zenitha Pinkan <hadanialaina@gmail.com>
2026-03-11 11:01:44 +0700

    Refactor layout for lantai detail view
    Removed redundant header section and adjusted layout.

--- resources/views/lantai/show.blade.php
+++ resources/views/lantai/show.blade.php
@@ -2,33 +2,21 @@
 
 @section('content')
 <div class="container">
+    <h2>Detail Lantai</h2>
 
-<<<<<<< HEAD
-    <div class="d-flex justify-content-between align-items-center mb-3">
-        <h2>Detail Lantai</h2>
-
-        <a href="{{ route('lantai.index') }}" class="btn btn-secondary">
-            <i class="fa-solid fa-arrow-left me-1"></i> Kembali
-        </a>
-    </div>
-
-=======
->>>>>>> d7302947f020310c79f6a86c9bbc92fdfa6339cf
     <ul class="list-group">
         <li class="list-group-item">
             <b>Gedung:</b> {{ $lantai->gedung->nama_gedung }}
         </li>
-
         <li class="list-group-item">
             <b>Kode Lantai:</b> {{ $lantai->kode_lantai }}
         </li>
-
         <li class="list-group-item">
             <b>Nama Lantai:</b> {{ $lantai->nama_lantai ?? '-' }}
         </li>
-
         <li class="list-group-item">
             <b>Status:</b>
+
             @if($lantai->is_active)
                 <span class="badge bg-success ms-2">
                     <i class="fa-solid fa-circle-check me-1"></i> Aktif
@@ -41,5 +29,8 @@
         </li>
     </ul>
 
+    <a href="{{ route('lantai.index') }}" class="btn btn-secondary mt-3">
+        <i class="fa-solid fa-arrow-left me-1"></i>Kembali
+    </a>
 </div>
 @endsection
